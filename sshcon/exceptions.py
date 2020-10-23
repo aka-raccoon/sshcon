83957901fef1f98e58b2851b43437d25d2f4f16f
racoonx2p <racoonx2p@gmail.com>
2020-10-23 14:07:38 +0200

    refactor: rewrite exceptions

--- sshcon/exceptions.py
+++ sshcon/exceptions.py
@@ -1,26 +1,19 @@
-class SshConNonZeroReturnCode(Exception):
-    def __init__(self, cmd, rcode, stderr):
-        self.cmd = cmd
-        self.rcode = rcode
-        self.stderr = stderr
-
-    def __str__(self):
-        return f"Command:{self.cmd}, RC:{self.rcode} -> {self.stderr}."
-
-
 class SshConSftpError(Exception):
     def __init__(self, cmd, rcode):
         self.cmd = cmd
         self.rcode = rcode
+        self.message = "Function has failed."
+        super().__init__(self.message)
 
     def __str__(self):
-        return f"Function:{self.cmd}, RC:{self.rcode} -> Function has failed."
+        return f"Function:{self.cmd}, RC:{self.rcode} -> {self.message}"
 
 
 class SshConError(Exception):
     def __init__(self, function, msg):
         self.function = function
         self.msg = msg
+        super().__init__(self.msg)
 
     def __str__(self):
         return f"Function:{self.function} -> {self.msg}"
