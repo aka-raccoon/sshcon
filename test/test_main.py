88e997ac0c25e48be52b71c0b4a15bf46503df95
racoonx2p <racoonx2p@gmail.com>
2020-10-24 15:16:56 +0200

    test: add new tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import pytest
 from sshcon.main import SshCon
@@ -11,6 +12,10 @@ remote = SshCon(host, user, key)
 
 TEMP_DIR = "/tmp/sshcon_test_dir"
 MADEUP_DIR = "/I/DO/NOT/EXIST/PYTHON/EGGS/BACON"
+REMOTE_FILENAME = "/tmp/sshcon_write.tst"
+DATA = "Hello, this is test file to check if writing is working in sshcon."
+APPEND_DATA = "PS: PYTHON IS BEST"
+LOCAL_FILE = Path("/tmp/sshcon_get_file.tst")
 
 
 def test_mkdir_exception():
@@ -54,3 +59,67 @@ def test_lstat():
 def test_umount_exception():
     with pytest.raises(PermissionError):
         remote.umount(MADEUP_DIR)
+
+
+def test_send_file():
+    filename = "/tmp/scp_sshcon.tst"
+    remote_file = Path("/tmp") / filename
+    with open(filename, "w") as file:
+        file.write("This is test file for sshcon!")
+    try:
+        remote.send_file(filename, remote_file)
+    except FileExistsError:
+        remote.remove(remote_file)
+        remote.send_file(filename, remote_file)
+
+
+def test_write_file():
+    try:
+        remote.write_text(DATA, REMOTE_FILENAME)
+    except FileExistsError:
+        remote.remove(REMOTE_FILENAME)
+        remote.write_text(DATA, REMOTE_FILENAME)
+
+
+def test_overwrite_data():
+    remote.write_text(DATA, REMOTE_FILENAME, force=True)
+
+
+def test_overwrite_data_exception():
+    with pytest.raises(FileExistsError):
+        remote.write_text(DATA, REMOTE_FILENAME, force=False)
+    remote.write_text(DATA, REMOTE_FILENAME, force=True)
+
+
+def test_append_data():
+    remote.write_text(APPEND_DATA, REMOTE_FILENAME, append=True)
+
+
+def test_read_text():
+    text = remote.read_text(REMOTE_FILENAME)
+    assert text == DATA + APPEND_DATA
+
+
+def test_get_file():
+    try:
+        remote.get_file(REMOTE_FILENAME, LOCAL_FILE)
+    except FileExistsError:
+        LOCAL_FILE.unlink()
+        remote.get_file(REMOTE_FILENAME, LOCAL_FILE)
+    with open(LOCAL_FILE, "r") as file:
+        text = file.read()
+    assert text == DATA + APPEND_DATA
+
+
+def test_get_file_nonforce():
+    with pytest.raises(FileExistsError):
+        remote.get_file(REMOTE_FILENAME, LOCAL_FILE, force=False)
+
+
+def test_get_file_force():
+    remote.get_file(REMOTE_FILENAME, LOCAL_FILE, force=True)
+    LOCAL_FILE.unlink()
+
+
+def test_remove():
+    remote.remove(REMOTE_FILENAME)
